d026881dc7c2f603fff042084a347e7a9dcb2212
Max <maxwell.githinji@student.moringaschool.com>
2022-12-02 02:17:44 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form= document.querySelector ('form');
+  form.addEventListener ('submit',
+ (e)=> {
+    e.preventDefault ();
+    let mySubmit = document.getElementById ('new-task-description');
+    newSubmit (mySubmit.value);
+    form.reset ();
+  });
+
 });
+
+function newSubmit (todo) {
+    //  console.log (todo);
+     let p = document.createElement ('p');
+     let btn = document.createElement ('button');
+     btn.addEventListener ('click', deleteTodoList)
+     btn.textContent= ('x')
+     p.textContent = `${todo} `;
+     document.querySelector ("#tasks").appendChild (p);
+     p.appendChild (btn);
+     console.log (p)
+}
+
+function deleteTodoList (e) {
+     e.target.parentNode.remove ()
+}
